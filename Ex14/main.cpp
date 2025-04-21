59af472db459ceef774070d406ebd5868b65db37
GustavoCurado <gustavocurado@usp.br>
2025-04-20 22:15:40 -0300

    Add exercise 14

---
+++ Ex14/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * Nome: Gustavo Curado Ribeiro 
+ * NUSP: 14576732
+ * SCC0210 - Laboratório de Algoritmos Avançados I
+ * Exercício 14: Fazendo Somas na Ilha de Yock
+*/
+
+#include <iostream>
+
+int main()
+{
+	return 0;
+}
