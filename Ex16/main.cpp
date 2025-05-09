7c0715686a710ca0578df5794f06c021a1c211e9
GustavoCurado <gustavocurado@usp.br>
2025-05-09 16:49:25 -0300

    Inicia exercício 16

---
+++ Ex16/main.cpp
@@ -0,0 +1,13 @@
+/*    
+ * Nome: Gustavo Curado Ribeiro     
+ * NUSP: 14576732    
+ * SCC0210 - Laboratório de Algoritmos Avançados I    
+ * Exercício 16: Brain Age – Divisibilidade Fatorial
+*/
+
+#include <iostream>
+
+int main()
+{
+	return 0;
+}
