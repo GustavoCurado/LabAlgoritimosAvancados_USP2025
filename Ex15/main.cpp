510b20999ef1cb71d317f06ec2584e4a01b57d56
GustavoCurado <gustavocurado@usp.br>
2025-05-09 14:52:48 -0300

    Inicia o exercício 15

---
+++ Ex15/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * Nome: Gustavo Curado Ribeiro 
+ * NUSP: 14576732
+ * SCC0210 - Laboratório de Algoritmos Avançados I
+ * Exercício 15: Brain Age – Fatoração em Primos
+*/
+
+#include <iostream>
+
+int main()
+{
+	return 0;
+}
+
